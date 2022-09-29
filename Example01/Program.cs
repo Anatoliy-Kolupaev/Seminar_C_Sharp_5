6717e1c5000706496604c5c7c5e587c790b1621f
Anatoliy K <778966@mail.ru>
2022-09-29 23:36:40 +0500

    added ex1

---
+++ Example01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Дополнительно: Написать программу которая из имен через запятую выберет случайное
+// имя и выведет в терминал
+// Игорь, Антон, Сергей -> Антон
+
+//string word = System.Console.ReadLine();
+//string[] num = word.Split(" ");     //команда которая дает разделить строку убрать пробелы
+//for (int i = 0; i < num.Length; i++)
+//{
+//    System.Console.WriteLine(num[i]);
+//}
+string words = Console.ReadLine();
+Console.WriteLine(words);
+string[] nums = words.Split(",");
+int index = new Random().Next(0,nums.Length);
+Console.WriteLine(nums[index]);
