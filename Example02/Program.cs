66cee47f16ed44c732a6d49c2c64f3bf4266dbdf
Anatoliy K <778966@mail.ru>
2022-09-29 23:36:50 +0500

    added ex2

---
+++ Example02/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] CreatArr(int size)
+{
+    int [] array = new int [size]; // задаем новый массив
+    for (int i = 0; i < array.Length; i++) // заполняем массив рандом числами от -10 до 11
+    {
+        array [i] = new Random().Next(-10,11);
+    }
+    return array;
+}
+int[] NumberArr (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]*=-1;  // делает цифры другим знаком
+    }
+    return array;
+}
+bool FindNumber(int number, int[] array) //  проверяет есть ли 1 в массиве
+{
+    //foreach (var item in array)
+    //{
+        //if(item == number) 
+       // {
+            //return true;
+        //}
+    //}
+    //return false;
+    return array.Contains(number); //  проверяет есть ли 1 в массиве только без цикла (поиск элементов)
+}
+
+
+int [] array = CreatArr(8);
+System.Console.WriteLine(string.Join(" ", array));
+array = NumberArr(array);
+Console.WriteLine(String.Join(" ",array));
+Console.WriteLine(FindNumber(1,array));
